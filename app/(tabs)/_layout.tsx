970eed36a8258eca9998b2350d398daef0c911e3
Braden Schleip <bschleip@wisc.edu>
2024-12-15 14:56:55 -0600

    initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,46 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
+    return (
+      <Tabs
+          screenOptions={{
+              tabBarActiveTintColor: '#ffd33d',
+              headerStyle: {
+                  backgroundColor: '#25292e',
+              },
+              headerShown: false,
+              headerTintColor: '#fff',
+              tabBarStyle: {
+                  backgroundColor: '#25292e',
+              },
+          }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Start',
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons name={focused ? 'add' : 'add-outline'} color={color} size={24} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons name={focused ? 'person-circle' : 'person-circle-outline'} color={color} size={24}/>
+            ),
+          }}
+        />
+        <Tabs.Screen
+            name='menu'
+            options={{
+                title: 'Menu'
+            }}
+        />
+      </Tabs>
+    );
+  }
+  
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,9 @@
-import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-import Button from '@/components/Button'
-
-export default function Index() {
+export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Button 
-        label='Create a workout'
-        onPress={()=>alert('button pressed')}
-      />
-      <Text style={styles.text}>Create workout above</Text>
+      <Text style={styles.text}>Profile screen</Text>
     </View>
   );
 }
